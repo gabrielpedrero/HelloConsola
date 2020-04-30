38d7ed7f5974cbeaa68a133c9ca614c864945e6f
Gabriel Pedrero <gabrielpedrero@gmail.com>
2020-04-30 10:14:09 -0500

    Comit inicial del proyecto.

---
+++ HelloConsola/HelloConsola/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola desde la aplicación de prueba HelloConsola...");
+            Console.WriteLine("Pulse <Enter> para terminar...");
+            Console.ReadLine();
+        }
+    }
+}
